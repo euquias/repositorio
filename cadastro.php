6f3931f588b61ea45140f8476b5b9ee5ff042fdb
Euquias Araujo <willmaglhaes25@gmail.com>
2021-07-11 12:24:41 -0300

    tela de login, sem o uso do php

---
+++ cadastro.php
@@ -0,0 +1,176 @@
+
+<html>
+
+<head>
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.0.26/css/bulma.css">
+     <meta charset="utf-8">
+     <title>euquias</title>
+</head>
+
+<section class="hero is-info is-small">
+  <div class="hero-head">
+    <nav class="navbar">
+      <div class="container">
+        <div class="navbar-brand">
+          <a class="navbar-item">
+            <img src="" alt="Logo">
+             </a>
+             <div class="navbar-end">
+            <a class="navbar-item is-active">
+              <a class="button is-danger">
+                home
+              </a>
+            </a>
+            <a class="navbar-item">
+              <a class="button is-danger">
+                cadastro
+              </a>
+            <a class="navbar-item">
+              <a class="button is-danger">
+                login
+              </a>
+        </div>
+      </div>
+    </nav>
+  </div>
+  <div class="hero-body">
+    <div class="container -centered">
+      <!DOCTYPE html>
+  <html>
+  <head>
+      <meta charset='utf-8'>
+      <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+      <title>tela de cadastro</title>
+      <meta name='viewport' content='width=device-width, initial-scale=1'>
+      <script src='main.js'></script>
+      
+      <form action= "progeto.php" method="post">
+        <div class="container is-small">
+          <div class="notification is-primary">
+  
+      <div class="field">
+          <label class="label">usuario</label>
+          <div class="control">
+            <input name= "usuario" type= 'text' placeholder= "usuario">
+            <label class="checkbox" disabled>
+              <input type="checkbox" disabled>
+            </label>
+            <h4>SEXO</h4>
+            <div class="select is-success">
+              <select> 
+                <option>MASCOLINO</option>
+                <option>FEMININO</option>
+                <option>INDEFINIDO</option>
+              </select>
+            </div>
+            </label>
+          </div>
+        </div>        
+        <div class="field">
+          <label class="label">data</label>
+          <div class="control has-icons-left has-icons-right">
+            <input name= "nascimento" type= "date" placeholder="nascimento">
+            <span class="icon is-small is-left">
+              <i class="fas fa-user"></i>
+            </span>
+            <span class="icon is-small is-right">
+              <i class="fas fa-check"></i>
+            </span>
+          </div>
+          <p class="help is-success"></p>
+        </div>
+        <div class="field">
+          <label class="label">telefone</label>
+          <div class="control">
+            <input name= "tel" type="number" placeholder="telefone">
+          </div>
+        </div>
+        <div class="field">
+          <label class="label">Email</label>
+          <div class="control has-icons-left has-icons-right">
+            <input name= "email" type="email" placeholder="Email@" value="">
+            <span class="icon is-small is-left">
+              <i class="fas fa-envelope"></i>
+            </span>
+            <span class="icon is-small is-right">
+              <i class="fas fa-exclamation-triangle"></i>
+            </span>
+          </div>
+        </div>       
+            <span class="icon is-small is-right">
+              <i class="fas fa-check"></i>
+            </span>
+          </p>
+        </div>
+        <div class="field">
+          <p class="control has-icons-left">
+            <input class="input" type="password" placeholder="Password">
+            <span class="icon is-small is-left">
+              <i class="fas fa-lock"></i>
+            </span>
+          </p>
+        </div>
+        <span class="icon is-small is-right">
+          <i class="fas fa-check"></i>
+        </span>
+      </p>
+    </div>
+    <div class="field">
+      <p class="control has-icons-left">
+        <input class="input" type="password" placeholder="Password">
+        <span class="icon is-small is-left">
+          <i class="fas fa-lock"></i>
+        </span>
+      </p>
+    </div>
+        <label class="checkbox">
+          <input type="checkbox">
+          li e concordo todos os termo: <a href="#">terms and conditions</a>
+        </label>
+       
+        <div class="field is-grouped">
+          <div class="control">
+          <input type= "submit" value= "enviar">
+        </div>
+        </div>
+      </div>
+  </from>
+  </head>
+  <body>  
+  </body>
+  </html>     
+  </table>
+    </div>
+    </div>
+  </div>
+     <div class="hero-foot">
+    <nav class="tabs is-boxed is-fullwidth">
+      <div class="container">
+        <ul>
+          <li class="is-active">
+            <a>Overview</a>
+          </li>
+          <li>
+            <a>Modifiers</a>
+          </li>
+          <li>
+            <a>Grid</a>
+          </li>
+          <li>
+            <a>Elements</a>
+          </li>
+          <li>
+            <a>Components</a>
+          </li>
+          <li>
+            <a>Layout</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+</section>
+ 
+
+
+
